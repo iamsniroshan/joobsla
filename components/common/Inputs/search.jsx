6825eb7921457be649d139af001d896282ac4306
selvaraja.niroshan <selvaraja.niroshan@un.org>
2022-05-17 13:21:59 +0530

    ISSUe is fixed

--- components/common/Inputs/search.jsx
+++ components/common/Inputs/search.jsx
@@ -1,40 +1,40 @@
 import { useState } from "react";
-import Select from "./select";
+
 
 const jobType = [
-    {label:'IT-Sware/DB/QA/Web/Graphics/GIS', value: '10',id:1},
-    {label:'IT-HWare/Networks/Systems', value:'20',id:'2'},
-    {label:'Accounting/Auditing/Finance', value:'100',id:'3'},
-    {label:'Banking/Insurance', value:'10', id:'4'},
-    {label:'Sales/Marketing/Merchandising', value:'58',id:'5'},
-    {label:'HR/Training', value:'58',id:'5'},
-    {label:'Corporate Management/Analysts', value:'58',id:'5'},
-    {label:'Office Admin/Secretary/Receptionist', value:'58',id:'5'},
-    {label:'Civil Eng/Interior Design/Architecture', value:'58',id:'5'},
-    {label:'IT-Telecoms', value:'58',id:'5'},
-    {label:'Customer Relations/Public Relations', value:'58',id:'5'},
-    {label:'Logistics/Warehouse/Transport', value:'58',id:'5'},
-    {label:'Eng-Mech/Auto/Elec', value:'58',id:'5'},
-    {label:'Manufacturing/Operations', value:'58',id:'5'},
-    {label:'Media/Advert/Communication', value:'58',id:'5'},
-    {label:'Hotels/Restaurants/Food', value:'58',id:'5'},
-    {label:'Hospitality/Tourism', value:'58',id:'5'},
-    {label:'Hospitality/Tourism', value:'58',id:'5'},
-    {label:'Sports/Fitness/Recreation', value:'58',id:'5'},
-    {label:'Hospital/Nursing/Healthcare', value:'58',id:'5'},
-    {label:'Legal/Law', value:'58',id:'5'},
-    {label:'Supervision/Quality Control', value:'58',id:'5'},
-    {label:'Apparel/Clothing', value:'58',id:'5'},
-    {label:'Ticketing/Airline/Marine', value:'58',id:'5'},
-    {label:'Teaching/Academic/Library', value:'58',id:'5'},
-    {label:'R&D/Science/Research', value:'58',id:'5'},
-    {label:'Agriculture/Dairy/Environment', value:'58',id:'5'},
-    {label:'Security', value:'58',id:'5'},
-    {label:'Fashion/Design/Beauty', value:'58',id:'5'},
-    {label:'International Development', value:'58',id:'5'},
-    {label:'KPO/BPO', value:'58',id:'5'},
-    {label:'Imports/Exports', value:'58',id:'5'},
-  ]
+    { label: 'IT-Sware/DB/QA/Web/Graphics/GIS', value: '10', id: 1 },
+    { label: 'IT-HWare/Networks/Systems', value: '20', id: '2' },
+    { label: 'Accounting/Auditing/Finance', value: '100', id: '3' },
+    { label: 'Banking/Insurance', value: '10', id: '4' },
+    { label: 'Sales/Marketing/Merchandising', value: '58', id: '5' },
+    { label: 'HR/Training', value: '58', id: '5' },
+    { label: 'Corporate Management/Analysts', value: '58', id: '5' },
+    { label: 'Office Admin/Secretary/Receptionist', value: '58', id: '5' },
+    { label: 'Civil Eng/Interior Design/Architecture', value: '58', id: '5' },
+    { label: 'IT-Telecoms', value: '58', id: '5' },
+    { label: 'Customer Relations/Public Relations', value: '58', id: '5' },
+    { label: 'Logistics/Warehouse/Transport', value: '58', id: '5' },
+    { label: 'Eng-Mech/Auto/Elec', value: '58', id: '5' },
+    { label: 'Manufacturing/Operations', value: '58', id: '5' },
+    { label: 'Media/Advert/Communication', value: '58', id: '5' },
+    { label: 'Hotels/Restaurants/Food', value: '58', id: '5' },
+    { label: 'Hospitality/Tourism', value: '58', id: '5' },
+    { label: 'Hospitality/Tourism', value: '58', id: '5' },
+    { label: 'Sports/Fitness/Recreation', value: '58', id: '5' },
+    { label: 'Hospital/Nursing/Healthcare', value: '58', id: '5' },
+    { label: 'Legal/Law', value: '58', id: '5' },
+    { label: 'Supervision/Quality Control', value: '58', id: '5' },
+    { label: 'Apparel/Clothing', value: '58', id: '5' },
+    { label: 'Ticketing/Airline/Marine', value: '58', id: '5' },
+    { label: 'Teaching/Academic/Library', value: '58', id: '5' },
+    { label: 'R&D/Science/Research', value: '58', id: '5' },
+    { label: 'Agriculture/Dairy/Environment', value: '58', id: '5' },
+    { label: 'Security', value: '58', id: '5' },
+    { label: 'Fashion/Design/Beauty', value: '58', id: '5' },
+    { label: 'International Development', value: '58', id: '5' },
+    { label: 'KPO/BPO', value: '58', id: '5' },
+    { label: 'Imports/Exports', value: '58', id: '5' },
+]
 
 export default function SearchComponent() {
 
@@ -48,7 +48,7 @@ export default function SearchComponent() {
         if (value.length > 0) {
             const regex = new RegExp(`^${value}`, 'i');
             suggestions = jobType.sort().filter(v => regex.test(v.label));
-        }else{
+        } else {
             suggestions = jobType
         }
         setSuggestion(suggestions)
@@ -74,15 +74,15 @@ export default function SearchComponent() {
     }
 
     return (
-      <>
-        <div>
-          {/* <label
+        <>
+            <div>
+                {/* <label
             htmlFor="name"
             className="block text-sm font-medium text-gray-700 pb-1"
           >
             Category
           </label> */}
-          <div className="flex mt-1 border-gray-300 focus-within:border-gray-300 relative">
+                <div className="flex mt-1 border-gray-300 focus-within:border-gray-300 relative">
                     <input
                         value={text}
                         onClick={onTextChanged}
@@ -93,16 +93,16 @@ export default function SearchComponent() {
                         placeholder="Jane Doe"
                     />
                     <label
-              for="floating_outlined"
-              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-800 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-            >Category</label>
+                        for="floating_outlined"
+                        class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-800 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                    >Category</label>
                     {suggestions.length > 0 && (<div className="absolute w-full py-1 border border-gray-300 mt-10 overflow-auto text-base bg-gray-100 rounded-md max-h-60  focus:outline-none sm:text-sm">
                         {renderSuggestions()}
                     </div>)}
                 </div>
 
-        </div>
-      </>
+            </div>
+        </>
     );
 }
 
