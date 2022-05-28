62d91f9d676b1760f8d8729bca963d7042b1f373
selvaraja.niroshan <selvaraja.niroshan@un.org>
2022-05-29 02:16:35 +0530

    heder

--- components/web/Post/tablelist.jsx
+++ components/web/Post/tablelist.jsx
@@ -116,6 +116,24 @@ export default function PostCardListComponent() {
                     {/* Main 1 column grid */}
                     <div className="grid grid-cols-1 gap-4 items-start post-list-height">
                     <div className="bg-white shadow overflow-hidden sm:rounded-md">
+                    <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+      <div className="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
+        <div className="ml-4 mt-4">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Job Postings</h3>
+          <p className="mt-1 text-sm text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit quam corrupti consectetur.
+          </p>
+        </div>
+        <div className="ml-4 mt-4 flex-shrink-0">
+          <button
+            type="button"
+            className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Create new job
+          </button>
+        </div>
+      </div>
+    </div>
       <ul className="divide-y divide-gray-200">
         {positions.map((position) => (
           <li key={position.id}>
