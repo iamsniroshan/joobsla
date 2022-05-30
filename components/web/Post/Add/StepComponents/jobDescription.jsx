563f155f8a2c847b0256ab9a7633d6e02e8d4fc6
selvaraja.niroshan <selvaraja.niroshan@un.org>
2022-05-30 14:04:27 +0530

    some more implementation

--- components/web/Post/Add/StepComponents/jobDescription.jsx
+++ components/web/Post/Add/StepComponents/jobDescription.jsx
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import OrderContext from '../OrderContext';
-
+import ContentEditable from 'react-contenteditable'
 
 
 const JobDescriptionComponent = () => {
 
     const { postDetails, setPostDetails } = useContext(OrderContext); // Context API
 
-    const handleInputChange = ({ target: { name, value } }) => {
+    const handleInputChange = ({ target: { name = 'desc', value } }) => {
         const data = { ...postDetails }
         data["jobDescription"][name] = value;
         setPostDetails(data);
@@ -16,29 +16,72 @@ const JobDescriptionComponent = () => {
     // Destructuring object from Context API
     const { jobDescription } = postDetails;
 
+
+
+    const handleChange = () => {
+        // const text = useRef('<p>Hello <b>World</b>!</p><p>Paragraph 2</p><ul><li>wefdwedewdw</li></ul>');
+        handleInputChange({ target: { name: 'desc', value: '<p>Hello <b>World</b>!</p><p>Paragraph 2</p><ul><li>wefdwedewdw</li></ul>' } })
+    };
+
+    const iconHtml = {
+        bold: <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-type-bold" viewBox="0 0 16 16">
+            <path d="M8.21 13c2.106 0 3.412-1.087 3.412-2.823 0-1.306-.984-2.283-2.324-2.386v-.055a2.176 2.176 0 0 0 1.852-2.14c0-1.51-1.162-2.46-3.014-2.46H3.843V13H8.21zM5.908 4.674h1.696c.963 0 1.517.451 1.517 1.244 0 .834-.629 1.32-1.73 1.32H5.908V4.673zm0 6.788V8.598h1.73c1.217 0 1.88.492 1.88 1.415 0 .943-.643 1.449-1.832 1.449H5.907z" />
+        </svg>,
+        italic: <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-type-italic" viewBox="0 0 16 16">
+            <path d="M7.991 11.674 9.53 4.455c.123-.595.246-.71 1.347-.807l.11-.52H7.211l-.11.52c1.06.096 1.128.212 1.005.807L6.57 11.674c-.123.595-.246.71-1.346.806l-.11.52h3.774l.11-.52c-1.06-.095-1.129-.211-1.006-.806z" />
+        </svg>,
+        bulletList: <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+        </svg>,
+        numberList: <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list-ol" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z" />
+            <path d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338v.041zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635V5z" />
+        </svg>,
+        html: <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-filetype-html" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+        </svg>,
+        hTag: <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-type-h1" viewBox="0 0 16 16">
+            <path d="M8.637 13V3.669H7.379V7.62H2.758V3.67H1.5V13h1.258V8.728h4.62V13h1.259zm5.329 0V3.669h-1.244L10.5 5.316v1.265l2.16-1.565h.062V13h1.244z" />
+        </svg>
+    }
+
+    const EditButton = (props) => {
+        return (
+            <button
+                type="button"
+                key={props.cmd}
+                onMouseDown={evt => {
+                    evt.preventDefault(); // Avoids loosing focus from the editable area
+                    document.execCommand(props.cmd, false, props.arg); // Send the command to the browser
+                }}
+                className="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+            >
+                {props.icon || props.name || props.cmd}
+            </button>
+        );
+    }
+
     return (
         <>
 
-                    <form>
-                        <div className="bg-white  py-10  h-96">
-                            <div className="grid grid-cols-6 gap-6">
-                                <div className="col-span-6 sm:col-span-3">
-                                    <label
-                                        htmlFor="first-name"
-                                        className="block text-sm font-medium text-gray-700"
-                                    >
-                                        Job Title
-                                    </label>
-                                    <input
-                                        type="text"
-                                        name="desc" value={jobDescription.desc} placeholder="Job description" onChange={handleInputChange}
-                                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    />
-                                </div>
-
-                            </div>
-                        </div>
-                    </form>
+            <form>
+                <div className="bg-white  py-10  h-96">
+                    <div className="grid grid-cols-6 gap-6">
+                        <EditButton cmd="italic" icon={iconHtml.italic} />
+                        <EditButton cmd="bold" icon={iconHtml.bold} />
+                        <EditButton cmd="formatBlock" arg="h6" name="heading" icon={iconHtml.bold} />
+                        <EditButton cmd="insertHTML" arg="<h2 className='mt-4 text-base font-medium text-gray-900'>test content</h2>" name="insertHTML" />
+                        <EditButton cmd="insertOrderedList" arg="" name="insertOrderedList" />
+                        <EditButton
+                            cmd="createLink"
+                            arg="https://github.com/lovasoa/react-contenteditable"
+                            name="hyperlink"
+                        />
+                        <button onClick={handleChange} type="button">cccccc</button>
+                    </div>
+                    <ContentEditable html={jobDescription.desc} onChange={handleInputChange} className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                </div>
+            </form>
         </>
     )
 }
