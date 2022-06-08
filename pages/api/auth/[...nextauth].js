19c72ad01478044b77eafc970d5660a1fb70171f
selvaraja.niroshan <selvaraja.niroshan@un.org>
2022-06-08 19:02:51 +0530

    improvement

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,7 @@
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/react';
-
 import { verifyPassword } from 'helpers/auth';
 import { connectToDatabase } from 'helpers/db';
-
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
   session: {
@@ -12,7 +10,7 @@ export default NextAuth({
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   providers: [
-    Providers.Credentials({
+    CredentialsProvider({
       name: "credentials",
       credentials: {
         username: { label: "username", type: "text" },
@@ -25,9 +23,10 @@ export default NextAuth({
         const usersCollection = client.db().collection('users');
 
         const user = await usersCollection.findOne({
-          email: credentials.email,
+          email: credentials.email
         });
-
+        console.log(user)
+        console.log('xxxxxxxxxxxxx')
         if (!user) {
           client.close();
           throw new Error('No user found!');
@@ -50,19 +49,14 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    jwt: async (token, user, account, profile, isNewUser) => {
-      //  "user" parameter is the object received from "authorize"
-      //  "token" is being send below to "session" callback...
-      //  ...so we set "user" param of "token" to object from "authorize"...
-      //  ...and return it...
-      user && (token.user = user);
-      return Promise.resolve(token)   // ...here
+    async session({ session, user, token }) {
+      session.accessToken = token.accessToken
+      console.log(session)
+      console.log(user)
+      return session
     },
-    session: async (session, user, sessionToken) => {
-      //  "session" is current session object
-      //  below we set "user" param of "session" to value received from "jwt" callback
-      session.user = user.user;
-      return Promise.resolve(session)
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token
     }
   },
 });
