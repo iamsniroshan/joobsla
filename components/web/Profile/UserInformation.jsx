bc623f752926c8cde113b07b40847ed270987b08
selvaraja.niroshan <selvaraja.niroshan@un.org>
2022-06-12 23:07:46 +0530

    ok

--- components/web/Profile/UserInformation.jsx
+++ components/web/Profile/UserInformation.jsx
@@ -46,7 +46,7 @@ export default function UserInformationComponent({userInfo,cv}) {
                 <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                   <div className="w-0 flex-1 flex items-center">
                     <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span className="ml-2 flex-1 w-0 truncate">{cv.fileName.split(/_(.*)/s)[1]}</span>
+                    <span className="ml-2 flex-1 w-0 truncate">{cv?.fileName.split(/_(.*)/s)[1]}</span>
                   </div>
                   <div className="ml-4 flex-shrink-0">
                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
