4783df542850736798229fe02e0f85d8b54ce701
selvaraja.niroshan <selvaraja.niroshan@un.org>
2023-05-04 01:20:32 +0530

    mmmmmmm

--- components/web/JobApply/index.jsx
+++ components/web/JobApply/index.jsx
@@ -1,13 +1,139 @@
 
 import { useSession, signOut } from 'next-auth/react';
+/* This example requires Tailwind CSS v2.0+ */
+import {
+  PaperClipIcon,
+  PencilAltIcon,
+  PencilIcon,
+} from "@heroicons/react/solid";
+import { useQuery } from 'react-query';
+import { getUserInfoApi } from 'services/api';
+import { useState } from 'react';
+import { format } from "date-fns";
+
+const people = [
+  {
+    name: 'Michael Foster',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  },
+  // More people...
+]
+
+export default function JobApplyComponent()  {
+
+  const [userDetail, setUserDetail] = useState({});
+
+    const { isLoading, error, data } = useQuery('userInfoUseQuery', () => getUserInfoApi(),{
+        onSuccess: (data) => setUserDetail(data.data[0])
+    });
+
+  const {data:session, status:loading } = useSession();
+  const { userInfo, cv, salary } = userDetail
 
 
-export default function JobApplyComponent() {
-    const {data:session, status:loading } = useSession();
+  if(!userInfo) {
+    return null
+  }
 
   return (
-      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 w-96">
-        job apply form
+    <>
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 ">
+        <div className="space-y-8 sm:space-y-12">
+          <ul className="mx-auto">
+            {people.map((person) => (
+              <li key={person.name}>
+                <div className="space-y-4">
+                  <img className="mx-auto h-20 w-20 rounded-full lg:w-24 lg:h-24" src={person.imageUrl} alt="" />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>{person.name}</h3>
+                      <p className="text-indigo-600">{person.role}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-  );
+    </div>
+    <div className="border-t border-gray-200">
+
+    {userInfo && (
+      <dl>
+        {userInfo.firstName && (
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">Full name</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {userInfo.firstName} {userInfo?.lastName}
+            </dd>
+          </div>
+        )}
+        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt className="text-sm font-medium text-gray-500">
+            Application for
+          </dt>
+          <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            Backend Developer
+          </dd>
+        </div>
+        {userInfo.emailAddress && (
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Email address
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {userInfo.emailAddress}
+            </dd>
+          </div>
+        )}
+        {salary.salaryExpectation && (
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Salary expectation
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {salary.salaryExpectation} {salary.currency}
+            </dd>
+          </div>
+        )}
+        {cv.fileName && (
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Attachments
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                  <div className="w-0 flex-1 flex items-center">
+                    <PaperClipIcon
+                      className="flex-shrink-0 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-2 flex-1 w-0 truncate">
+                      {cv.fileName.split(/_(.*)/s)[1]}
+                    </span>
+                  </div>
+                  <div className="ml-4 flex-shrink-0">
+                    <a
+                      href="#"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Download
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </dd>
+          </div>
+        )}
+      </dl>
+    )}
+  </div>
+  </>
+  )
 }
+
