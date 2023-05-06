fc2784050d9de63537aa6acdb263c24ee9262026
selvaraja.niroshan <selvaraja.niroshan@un.org>
2023-05-06 12:31:03 +0530

    done

--- components/web/JobProviderApplications/KanbanBoard.jsx
+++ components/web/JobProviderApplications/KanbanBoard.jsx
@@ -6,20 +6,20 @@ import getAppliedJobPostApi from "services/api/getAppliedJobPost";
 import { useQuery } from "react-query";
 import { useSession, signOut } from 'next-auth/react';
 import updateApplicationStatusApi from "services/api/updateJobApplicationStatus";
-
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 
 
 
 export default function KanbanBoardComponent() {
   const [tasks, setTasks] = useState([]);
-  const [isClickedTileId, setClickedTileId] = useState('');
+  const [isDragDisabled, setIsDragDisabled] = useState(false);
   const { data: session, status: loading } = useSession();
   const { isLoading, error, data } = useQuery('appliedJobPostUseQuery', () => getAppliedJobPostApi());
 
   useEffect(() => {
     if (data && session) {
-      const newArr = data.data.map(e => {
+      const newArr = data.data.map((e,i) => {
         return {
           _id: e._id,
           jobTitle: e.jobDetail.jobTitle,
@@ -29,35 +29,55 @@ export default function KanbanBoardComponent() {
           applicationId: e.jobApplications.find(e => e.applicationUserId === session.user.id)._id,
           updatedAt: e.jobApplications.find(e => e.applicationUserId === session.user.id).updatedAt,
           createdAt: e.jobApplications.find(e => e.applicationUserId === session.user.id).createdAt,
+          order: i
         }
       })
+      newArr.sort((a, b) => a.order - b.order);
       setTasks([...newArr]);
     }
   }, [data, session]);
 
-  const onDragOver = (ev) => {
-    ev.preventDefault();
-  };
 
-  const onDragStart = (ev, jobTitle, id) => {
-    setClickedTileId(id)
-    ev.dataTransfer.setData("id", id);
-  };
-
-  const onDrop = (ev, cat) => {
-    setClickedTileId('')
-    const id = ev.dataTransfer.getData("id");
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+    const newArr = moveArrayObject(tasks, result);
     let draggedObj = {}
-    let fTasks = tasks.filter((task) => {
-      if (task._id == id) {
-        task.applicationStatus = cat;
+    let fTasks = newArr.filter((task) => {
+      if (task._id == result.draggableId) {
+        task.applicationStatus = result.destination.droppableId;
         draggedObj = task
       }
       return task;
     });
+    fTasks.sort((a, b) => a.order - b.order);
     updateApplicationStatusApiHandler(draggedObj);
     setTasks([...fTasks]);
   };
+
+  function moveArrayObject(arr, result) {
+    if(result.destination.droppableId === result.source.droppableId) {
+      const destinationArr = arr.filter(e => e.applicationStatus === result.destination.droppableId)
+      const remainingArr = arr.filter(e => e.applicationStatus !== result.destination.droppableId)
+      const [removedElement] = destinationArr.splice(result.source.index, 1);
+      destinationArr.splice(result.destination.index, 0, removedElement);
+      const comArr = [...destinationArr, ...remainingArr]
+      comArr.map((e,index) => e.order = index)
+      return comArr;
+    } else {
+      const destinationArr = arr.filter(e => e.applicationStatus === result.destination.droppableId)
+      const sourceArr = arr.filter(e => e.applicationStatus === result.source.droppableId)
+      const remainingArr = arr.filter(e => e.applicationStatus !== result.source.droppableId && e.applicationStatus !== result.destination.droppableId)
+      const [removedElement] = sourceArr.splice(result.source.index, 1);
+      destinationArr.splice(result.destination.index, 0, removedElement);
+      const comArr = [...destinationArr,...sourceArr, ...remainingArr]
+      comArr.map((e,index) => e.order = index)
+      return comArr;
+    }
+  }
+  
+
 
   const updateApplicationStatusApiHandler = (draggedObj) => {
     updateApplicationStatusApi(draggedObj).then(item => {
@@ -69,12 +89,6 @@ export default function KanbanBoardComponent() {
     });
   }
 
-  const handleKeyPress = (ev) => {
-    if (ev.key == "Enter" && ev.target.value != "") {
-      setTasks([...tasks, { jobTitle: ev.target.value, applicationStatus: "todo" }]);
-      ev.target.value = " ";
-    }
-  };
 
   const tasksArr = {
     applied: [],
@@ -85,43 +99,68 @@ export default function KanbanBoardComponent() {
   };
 
   tasks.forEach((t) => {
-    tasksArr[t.applicationStatus].push(
-      <div
-        className={`item-container ${isClickedTileId === t._id ? "opacity-50 border-2 border-dark-blue-500  rounded-md" : ""}`}
-        key={t.jobTitle}
-        draggable
-        onDragStart={(e) => onDragStart(e, t.name, t._id)}
-      >
-        <Card componentToPassDown={t} />
-      </div>
-    );
+    tasksArr[t.applicationStatus].push(t);
   });
-
 
 
   return (
     <>
-      <div className="flex flex-col h-full">
-        <KanbanFilterComponent />
-        <div className="-mt-24 flex  kanban-height max-w-7xl mx-auto px-4 sm:px-6 lg:px-1 bg-white rounded-lg">
-          <main className="flex-1 overflow-auto">
-            <div className="inline-flex h-full p-3 space-x-3 overflow-hidden">
-              {Object.keys(tasksArr).map((item, i) =>
-                <div
-                  key={i}
-                  onDragOver={(e) => onDragOver(e)}
-                  onDrop={(e) => onDrop(e, item)}
-                  className="flex flex-col flex-shrink-0 bg-gray-100 rounded-md w-60"
-                >
-                  <Column.Header>{item}</Column.Header>
-                  <Column.Body>{tasksArr[item]}</Column.Body>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="flex flex-col h-full">
+          <div className="flex flex-col h-full">
+            <KanbanFilterComponent />
+            <div className="-mt-24 flex  kanban-height max-w-7xl mx-auto px-4 sm:px-6 lg:px-1 bg-white rounded-lg">
+              <main className="flex-1 overflow-auto">
+                <div className="inline-flex h-full p-3 space-x-3 overflow-hidden">
+                  {Object.keys(tasksArr).map((item, i) =>
+                    <div
+                      key={i}
+                      className="flex flex-col flex-shrink-0 bg-gray-100 rounded-md w-60"
+                    >
+                      <Column.Header>{item}</Column.Header>
+                      <Column.Body>
+                        <Droppable droppableId={item} key={item}>
+                          {(provided, snapshot) => (
+                            <div {...provided.droppableProps} ref={provided.innerRef} style={{
+                              borderRadius: '0.5rem',
+                              minHeight: 'calc(100vh - 225px)',
+                              borderRadius: '4px',
+                              backgroundColor: snapshot.isDraggingOver ? '#f8fafc' : '',
+                            }}>
+                              {tasksArr[item].map((t, index) => (
+                                <Draggable key={t._id} draggableId={t._id} index={index} isDragDisabled={isDragDisabled}>
+                                  {(provided) => (
+                                    <div
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      ref={provided.innerRef}
+                                      className="my-3"
+                                    >
+                                      <div
+                                        className={`item-container bg-white rounded-lg ${snapshot.draggingFromThisWith === t._id
+                                          ? "bg-amber-50"
+                                          : ""
+                                          }`}
+                                      >
+                                        <Card componentToPassDown={t} />
+                                      </div>
+                                    </div>
+                                  )}
+                                </Draggable>
+                              ))}
+                              {provided.placeholder}
+                            </div>
+                          )}
+                        </Droppable>
+                      </Column.Body>
+                    </div>
+                  )}
                 </div>
-              )}
+              </main>
             </div>
-          </main>
-
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     </>
   );
 }
--- components/web/JobProviderApplications/SubComponents/card.js
+++ components/web/JobProviderApplications/SubComponents/card.js
@@ -1,18 +1,27 @@
+import { ClockIcon } from "@heroicons/react/outline";
+import Date from "components/common/Date";
+import DateDiff from "components/common/DateDiff";
+
 export default function Card(props) {
 	return (
-		<a className='transition block p-5 bg-white rounded-md shadow cursor-move ghost-card'>
+		<a className='transition block p-5 rounded-md shadow cursor-move ghost-card'>
 			<div className='flex items-center justify-between space-x-4'>
-				<span className='text-sm text-gray-900 truncate'>
-					{props.componentToPassDown.jobTitle}
-				</span>
-			</div>
-			<div className='flex items-center justify-between space-x-4'>
-				<span className='text-sm font-medium leading-snug'>{props.componentToPassDown.createdAt}</span>
+				<span className="inline-flex text-sm text-gray-500 items-center justify-between">
+                                <ClockIcon className="h-4 w-4 font-light" aria-hidden="true" />
+                                <span className="font-light text-sm text-gray-500 pl-2">
+                                  <DateDiff isoDate={props.componentToPassDown.createdAt} />
+                                </span>
+                            </span>
 				<img
 					className='w-14 h-6 object-contain'
 					src='https://looka.com/wp-content/themes/looka/images/logos/looka_logo_black.svg'
 					alt=''
 				/>
+			</div>
+			<div className='flex items-center justify-between space-x-4 py-2'>
+				<span className='text-sm text-gray-900 truncate'>
+					{props.componentToPassDown.jobTitle}
+				</span>
 			</div>
 			<p class="text-sm text-gray-700 overflow-hidden h-14">
 				{props.componentToPassDown.sortDesc}
@@ -21,8 +30,8 @@ export default function Card(props) {
 				<p className='text-xs px-2 dark:text-neutral-300 text-green-700 bg-green-100 rounded'>
 					{props.componentToPassDown.jobType}
 				</p>
-				<a href="#" class="flex text-gray-700">
-					<svg fill="none" viewBox="0 0 24 24" class="w-6 h-6 text-blue-500" stroke="currentColor">
+				<a href="#" class="flex text-gray-700 text-xs">
+					<svg fill="none" viewBox="0 0 24 24" class="w-5 h-5 text-blue-500" stroke="currentColor">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
 					</svg>
 					5
